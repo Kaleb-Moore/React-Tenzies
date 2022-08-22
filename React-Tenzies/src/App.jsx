b901268f6834dd1d5a3cdc3b7353a3bb681cf5fe
Kaleb Moore <k4l3bmoore@gmail.com>
2022-08-22 11:19:00 -0600

    Create Container for React game

--- React-Tenzies/src/App.jsx
+++ React-Tenzies/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function App() {
     return (
-        <h1>Hello World!</h1>
+        <main>
+            <div className="container"></div>
+        </main>
     )
 }
