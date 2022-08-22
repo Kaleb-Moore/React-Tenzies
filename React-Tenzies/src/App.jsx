b49fb71b358cf31b893d4c3caa9bf75dc2f47d40
Kaleb Moore <k4l3bmoore@gmail.com>
2022-08-22 12:18:21 -0600

    Create Function to get 10 random dice rolls

--- React-Tenzies/src/App.jsx
+++ React-Tenzies/src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Die from "./Die.jsx"
 
 export default function App() {
+
+    function allNewDice() {
+        const numArray = Array.from({length: 10}, () => Math.floor(Math.random() * 6 + 1));
+    }
+
     return (
         <main>
             <div className="dice--container">
