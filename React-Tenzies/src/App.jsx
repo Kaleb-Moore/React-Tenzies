093465329996bc0be396e90ba6ed16b2cf239cbf
Kaleb Moore <k4l3bmoore@gmail.com>
2022-08-22 13:47:22 -0600

    Update updateDice to only roll new numbers for the dice we don't want

--- React-Tenzies/src/App.jsx
+++ React-Tenzies/src/App.jsx
@@ -7,21 +7,31 @@ export default function App() {
 
     const [dice, setDice] = useState(allNewDice());
 
-    function allNewDice() {
-        return [...Array(10).fill(0).map(() => ({ 
+    function newDie () {
+        return { 
             value: `${Math.floor(Math.random() * 6 + 1)}`, 
             isHeld: false,
             key: nanoid(),
-        }))]
+        }
+    }
+
+    function allNewDice() {
+        return [...Array(10).fill(0).map(() => (newDie()))]
     }
 
     function updateDice() {
-        setDice(allNewDice())
+        setDice(oldDice => oldDice.map(die => {
+            return die.isHeld ?
+                die :
+                newDie()
+        }))
     }
 
     function holdDice(id) {
         setDice(oldDice => oldDice.map(die => {
-            return die.key === id ? {...die, isHeld: !die.isHeld} : die;
+            return die.key === id ? 
+                {...die, isHeld: !die.isHeld} : 
+                die;
         }))
     }
 
@@ -36,6 +46,8 @@ export default function App() {
     
     return (
         <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice--container">
                 {diceElements}
             </div>
