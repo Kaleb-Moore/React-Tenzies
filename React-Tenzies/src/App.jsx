0742935af54cedf9e433dae9e4b56217d79ffc34
Kaleb Moore <k4l3bmoore@gmail.com>
2022-08-22 12:00:03 -0600

    Create Die component and render to screen

--- React-Tenzies/src/App.jsx
+++ React-Tenzies/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Die from "./Die.jsx"
 
 export default function App() {
     return (
         <main>
-            
+            <div className="container">
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+            </div>
         </main>
     )
 }
